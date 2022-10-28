4e9fbf2fbd38c70248dfb2797423cda8198aa685
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-10-28 20:52:07 +0200

    fix: rounded context menu

--- components/ContextMenu.vue
+++ components/ContextMenu.vue
@@ -2,7 +2,7 @@
   <teleport to='body'>
     <div class="fixed w-screen h-screen top-0 left-0"></div>
     <div ref="target" class="bg-transparent rounded-xl shadow fixed top-0 left-0 flex flex-col text-start items-start" :style="pos">
-      <button class="[&:nth-child(1)]:rounded-b-none [&:last-child]:rounded-t-none btn btn-sm bg-neutral hover:bg-neutral-focus text-base text-start flex-1 w-32 justify-start" v-for="a in actions" :key="a.name" @click="a.fn">{{ a.name }}</button>
+      <button class="[&:nth-child(1)]:rounded-b-none [&:last-child]:rounded-t-none btn btn-sm bg-neutral hover:bg-neutral-focus text-base text-start flex-1 w-32 justify-start rounded-xl" v-for="a in actions" :key="a.name" @click="a.fn">{{ a.name }}</button>
     </div>
   </teleport>
 </template>
