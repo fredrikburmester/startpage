96e51e0af093871afbd7849c7218e8da1c72d900
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-11-01 22:47:35 +0100

    feat: more images

--- components/ImageComponent.vue
+++ components/ImageComponent.vue
@@ -16,7 +16,15 @@ const images = ['https://mj-gallery.com/2812b085-5451-4cfe-a511-b797a8adb6fb/gri
 'https://mj-gallery.com/83283aaa-44db-4f5c-b5e9-8a5aa5559a9d/grid_0.png',
 'https://mj-gallery.com/f7b965ff-71eb-4e75-b788-1651c20cdcd5/grid_0.png',
 'https://mj-gallery.com/c4bd6b31-5824-4f86-a6a1-9c4408f7c091/grid_0.png',
-'https://mj-gallery.com/c30016e1-ca57-4b19-840b-21b5e9c05a83/grid_0.png',]
+'https://mj-gallery.com/c30016e1-ca57-4b19-840b-21b5e9c05a83/grid_0.png',
+'https://mj-gallery.com/b2f7ecab-725a-476b-9e6d-d08f535f0d4e/grid_0.png',
+'https://mj-gallery.com/47ef2540-f911-41f6-8a68-75b49be79a2d/grid_0.png',
+'https://mj-gallery.com/f2e9fd82-a879-4c63-8827-a0e316d669f9/grid_0.png',
+'https://mj-gallery.com/473c2fdd-8207-4ddb-bd20-ffa0833cdca2/grid_0.png',
+'https://mj-gallery.com/8009e2a6-062a-4057-bbf6-bdcb0508d558/grid_0.png',
+'https://mj-gallery.com/b0129365-3bac-453b-a198-9bc3ce176b98/grid_0.png',
+'https://mj-gallery.com/0c6b3646-c95e-4e10-b486-3276c2cd0a7b/grid_0.png',
+]
 
 const store = useSettingsStore()
 
